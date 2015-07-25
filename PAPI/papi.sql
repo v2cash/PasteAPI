10369c5539f6831acd138b2b40fa5d4add8a45e5
Dan <StandOrFall@hotmail.co.uk>
2015-07-25 19:48:06 +0100

    Database Fix

--- PAPI/papi.sql
+++ PAPI/papi.sql
@@ -1,12 +1,11 @@
-/*
-	    ____  ___    ____  ____
-	   / __ \/   |  / __ \/  _/
-      / /_/ / /| | / /_/ // /  
-     / ____/ ___ |/ ____// /   
-	/_/   /_/  |_/_/   /___/   
-	Paste API By 2cash
-
-*/
+-- phpMyAdmin SQL Dump
+-- version 4.0.10.7
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jul 25, 2015 at 08:46 PM
+-- Server version: 5.5.42-cll
+-- PHP Version: 5.4.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -15,22 +14,28 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `standorf_papi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pastes`
+--
 
 CREATE TABLE IF NOT EXISTS `pastes` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `pasteid` text NOT NULL,
   `title` text NOT NULL,
   `content` text NOT NULL,
   `ip` varchar(11) NOT NULL,
-  `date` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `date` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
-ALTER TABLE `pastes`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `pastes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
